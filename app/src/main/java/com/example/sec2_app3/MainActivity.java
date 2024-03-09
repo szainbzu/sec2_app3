3da0a3a5b76213b1c8a1e3707ac6a9feb32092a8
szainbzu <szain@birzeit.edu>
2024-03-09 13:17:26 +0200

    version1

--- app/src/main/java/com/example/sec2_app3/MainActivity.java
+++ app/src/main/java/com/example/sec2_app3/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO: fill this feature
+        //this is another comment
     }
 }
