3bd51cff66e334f3b85970abb0eba77b7adde4e2
szainbzu <szain@birzeit.edu>
2024-03-09 13:14:12 +0200

    version1

---
+++ app/src/main/java/com/example/sec2_app3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sec2_app3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO: fill this feature
+    }
+}
